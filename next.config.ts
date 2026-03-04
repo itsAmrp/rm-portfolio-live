0882af25b1b8f5f1c032cc9d0d3f6fffa467bd19
Roshan Mariadas <roshanmariadas@Roshans-MacBook-Pro-OG.local>
2026-03-04 22:00:48 +0530

    fix: allow cloudinary image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/ddxa0ahzp/**",
+      },
     ],
   },
 };
